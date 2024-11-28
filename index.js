1c92f76c229bb89dc483bce7aa41b973c8a1e04c
Mayank Singh <mayank757511@gmail.com>
2024-11-28 20:15:42 +0530

    Final Changes

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const schoolRoutes = require('./routes/schoolRoutes');
-const mysql = require('mysql2');
+const db = require('./db/db')
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
 
 db.connect((err) => {
   if (err) {
@@ -30,4 +22,3 @@ app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
 
-module.exports = db; 
